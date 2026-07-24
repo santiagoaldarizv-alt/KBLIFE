f8cd7e085e37d5a03dab1a84c2201b68b17174ba
santiagoaldarizv-alt <santiagoaldarizv@gmail.com>
2026-07-24 17:06:58 +0200

    Fix PWA install: sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
 const CACHE = 'kblife-v6';
-const ASSETS = ['/'];
+const ASSETS = ['/KBLIFE/', '/KBLIFE/index.html', '/KBLIFE/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
@@ -18,7 +26,7 @@ self.addEventListener('fetch', e => {
         const clone = resp.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
         return resp;
-      }).catch(() => caches.match('/'));
+      }).catch(() => caches.match('/KBLIFE/'));
     })
   );
 });
